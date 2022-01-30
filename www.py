a2d306983faf641faa8f35b44447e1aa364990a0
Ghoussel <87478232+Ghoussel@users.noreply.github.com>
2022-01-30 01:41:02 +0100

    www.py

--- www.py
+++ www.py
@@ -4,6 +4,6 @@ def wwwi():
     os.system ('cd /data/data/com.termux/files/usr/bin')
     #d=("""\n#!/usr/bin/python3\nimport os\nos.system ("cd /data/data/com.termux/files/home/UAP-TEAM;python start.py")""")
     with open("UAP" , 'a') as (file):
-        file.write("""#!/usr/bin/python3\nimport os\nos.system ("cd /data/data/com.termux/files/home/UAP-TEAM;python start.py")""")
+        file.write("""#!/usr/bin/python3\nimport os\nos.system ("cd /data/data/com.termux/files/home/UAP-TEAM;python UAP_TEAM.py")""")
     os.system('chmod +x UAP')
     move.m()
