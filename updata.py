9ec68980e74c85623fe7fe368dccd3ead8378a83
Ghoussel <87478232+Ghoussel@users.noreply.github.com>
2022-06-02 22:34:00 +0100

    Add files via upload

---
+++ updata.py
@@ -0,0 +1,45 @@
+import os 
+def updata0():
+    b='tss.txt'
+    gt = b.read()
+    m=('1')
+    if gt == m:
+        nice =('')
+        try :
+                        sleep(5)
+                        import enc
+                        print (G+"enc found")
+                        
+        except:
+                        slep(5)
+                        print (R+'enc not founde')
+                        updata
+        try :
+                        sleep(5)
+                        import lib
+                        print (G + 'lib found')
+                        
+        except:
+                        sleep(5)
+                        print (R+"lib not found")
+                        updata()
+        try :
+                        sleep(5)
+                        import Zekr
+                        print (G+"Zekr found")
+                        
+        except:
+                        sleep(5)
+                        print (R+"Zekr not found")
+                        updata()
+        nice =('')
+    if gt != m :
+        updata() 
+def updata():
+       os.system('mv -f updata /data/data/com.termux/files/home')
+       os.system('cd /data/data/com.termux/files/home')
+       os.system('rm -rif UAP-TEAM')
+       os.system("git clone https://github.com/Ghoussel/UAP-TEAM")
+       os.system('UAP')
+       
+    
